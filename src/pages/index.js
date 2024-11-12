0b38405be30b868b3902afaef7691d792cfcfafb
david <vietdq@leadsgen.asia>
2024-11-12 19:53:35 +0700

    add login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,36 @@ import StartIcon from '@mui/icons-material/Start';
 import ExcelJS from 'exceljs';
 
 export default function Home() {
+    // Trạng thái xác thực
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loginError, setLoginError] = useState('');
+
+    // Tài khoản đăng nhập được cấp sẵn
+    const predefinedUsername = 'user123';
+    const predefinedPassword = 'password123';
+
+    // Trạng thái của form đăng nhập
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Các trạng thái khác
     const [selectedFiles, setSelectedFiles] = useState([]);
     const [progress, setProgress] = useState(0);
     const [outputFile, setOutputFile] = useState(null);
-    const [fileName, setFileName] = useState("Results"); // Tên file mặc định
+    const [fileName, setFileName] = useState('Results'); // Tên file mặc định
 
+    // Xử lý đăng nhập
+    const handleLogin = (e) => {
+        e.preventDefault();
+        if (username === predefinedUsername && password === predefinedPassword) {
+            setIsAuthenticated(true);
+            setLoginError('');
+        } else {
+            setLoginError('Invalid username or password.');
+        }
+    };
+
+    // Xử lý tải lên file
     const handleFileUpload = (e) => {
         const files = Array.from(e.target.files).filter(
             (file) => file.name.endsWith('.RPT')
@@ -26,6 +51,7 @@ export default function Home() {
         alert(`${files.length} files selected.`);
     };
 
+    // Xử lý quá trình xử lý file
     const handleProcess = async () => {
         if (selectedFiles.length === 0) {
             alert('Please select files first!');
@@ -66,7 +92,11 @@ export default function Home() {
         // Định dạng tiêu đề cho các cột đặc biệt
         headers.forEach((header, colIndex) => {
             const cell = headerRow.getCell(colIndex + 1);
-            if (!header.startsWith('SS') && !header.startsWith('STT') && !header.startsWith('Tên')) {
+            if (
+                !header.startsWith('SS') &&
+                !header.startsWith('STT') &&
+                !header.startsWith('Tên')
+            ) {
                 cell.font = { color: { argb: 'FFFF0000' } }; // Đặt màu đỏ cho tiêu đề
             }
         });
@@ -87,6 +117,7 @@ export default function Home() {
         alert('Excel file generated. You can download it below.');
     };
 
+    // Hàm xử lý từng file
     const processFile = (stt, content, assignedEnergies) => {
         let sampleName = '';
         const results = { STT: stt, 'Tên mẫu': '' };
@@ -115,7 +146,15 @@ export default function Home() {
                 const regex = /^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/;
                 const match = line.match(regex);
                 if (match) {
-                    const [_, nuclide, confidence, energy, yield_, activity, uncertainty] = match;
+                    const [
+                        _,
+                        nuclide,
+                        confidence,
+                        energy,
+                        yield_,
+                        activity,
+                        uncertainty,
+                    ] = match;
                     if (isFloat(energy.replace('*', ''))) {
                         const energyVal = parseFloat(energy.replace('*', ''));
                         currentNuclide = nuclide;
@@ -123,18 +162,28 @@ export default function Home() {
                         if (assignedEnergies[currentNuclide]) {
                             const assignedEnergy = assignedEnergies[currentNuclide];
                             if (Math.abs(energyVal - assignedEnergy) < 1) {
-                                if (parseFloat(activity) >= 0 && parseFloat(uncertainty) >= 0) {
+                                if (
+                                    parseFloat(activity) >= 0 &&
+                                    parseFloat(uncertainty) >= 0
+                                ) {
                                     results[`${currentNuclide}, Bq/kg`] = parseFloat(activity);
-                                    results[`SS, Bq/kg (${currentNuclide})`] = parseFloat(uncertainty);
+                                    results[`SS, Bq/kg (${currentNuclide})`] = parseFloat(
+                                        uncertainty
+                                    );
                                 } else {
                                     results[`${currentNuclide}, Bq/kg`] = '';
                                     results[`SS, Bq/kg (${currentNuclide})`] = '';
                                 }
                             }
                         } else {
-                            if (parseFloat(activity) >= 0 && parseFloat(uncertainty) >= 0) {
+                            if (
+                                parseFloat(activity) >= 0 &&
+                                parseFloat(uncertainty) >= 0
+                            ) {
                                 results[`${currentNuclide}, Bq/kg`] = parseFloat(activity);
-                                results[`SS, Bq/kg (${currentNuclide})`] = parseFloat(uncertainty);
+                                results[`SS, Bq/kg (${currentNuclide})`] = parseFloat(
+                                    uncertainty
+                                );
                             } else {
                                 results[`${currentNuclide}, Bq/kg`] = '';
                                 results[`SS, Bq/kg (${currentNuclide})`] = '';
@@ -150,62 +199,120 @@ export default function Home() {
 
     return (
         <Container maxWidth="md" sx={{ mt: 5 }}>
-            <Typography variant="h4" gutterBottom>
-                Sample Processor
-            </Typography>
-
-            <Box sx={{ my: 2 }}>
-                <Button variant="contained" component="label" startIcon={<UploadFileIcon />}>
-                    Choose Files
-                    <input type="file" hidden multiple onChange={handleFileUpload} accept=".RPT" />
-                </Button>
-                <Typography variant="body1" sx={{ mt: 1 }}>
-                    {selectedFiles.length > 0
-                        ? `${selectedFiles.length} file(s) selected.`
-                        : 'No files selected.'}
-                </Typography>
-            </Box>
-
-            <Box sx={{ my: 2 }}>
-                <TextField
-                    label="File Name"
-                    variant="outlined"
-                    value={fileName}
-                    onChange={(e) => setFileName(e.target.value)}
-                    fullWidth
-                />
-            </Box>
-
-            <Box sx={{ my: 2 }}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleProcess}
-                    startIcon={<StartIcon />}
-                >
-                    Start Processing
-                </Button>
-            </Box>
-
-            <Box sx={{ my: 2 }}>
-                <LinearProgress variant="determinate" value={progress} />
-                <Typography variant="body2" color="textSecondary">
-                    {progress}%
-                </Typography>
-            </Box>
-
-            {outputFile && (
-                <Box sx={{ my: 2 }}>
-                    <Button
-                        variant="contained"
-                        color="success"
-                        startIcon={<SaveAltIcon />}
-                        href={outputFile}
-                        download={`${fileName}.xlsx`}
-                    >
-                        Download Excel File
-                    </Button>
+            {!isAuthenticated ? (
+                // Form đăng nhập
+                <Box sx={{ my: 5 }}>
+                    <Typography variant="h4" gutterBottom>
+                        Đăng nhập
+                    </Typography>
+                    <form onSubmit={handleLogin} autoComplete="off">
+                        <TextField
+                            label="Username"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            InputLabelProps={{ shrink: true }}
+                            autoComplete="off"
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            InputLabelProps={{ shrink: true }}
+                            autoComplete="off"
+                        />
+                        {loginError && (
+                            <Typography color="error" variant="body2">
+                                {loginError}
+                            </Typography>
+                        )}
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            sx={{ mt: 2 }}
+                        >
+                            Đăng nhập
+                        </Button>
+                    </form>
                 </Box>
+            ) : (
+                // Ứng dụng chính
+                <>
+                    <Typography variant="h4" gutterBottom>
+                        Sample Processor
+                    </Typography>
+
+                    <Box sx={{ my: 2 }}>
+                        <Button
+                            variant="contained"
+                            component="label"
+                            startIcon={<UploadFileIcon />}
+                        >
+                            Chọn tệp
+                            <input
+                                type="file"
+                                hidden
+                                multiple
+                                onChange={handleFileUpload}
+                                accept=".RPT"
+                            />
+                        </Button>
+                        <Typography variant="body1" sx={{ mt: 1 }}>
+                            {selectedFiles.length > 0
+                                ? `${selectedFiles.length} tệp được chọn.`
+                                : 'Chưa chọn tệp nào.'}
+                        </Typography>
+                    </Box>
+
+                    <Box sx={{ my: 2 }}>
+                        <TextField
+                            label="Tên file"
+                            variant="outlined"
+                            value={fileName}
+                            onChange={(e) => setFileName(e.target.value)}
+                            fullWidth
+                        />
+                    </Box>
+
+                    <Box sx={{ my: 2 }}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleProcess}
+                            startIcon={<StartIcon />}
+                        >
+                            Bắt đầu xử lý
+                        </Button>
+                    </Box>
+
+                    <Box sx={{ my: 2 }}>
+                        <LinearProgress variant="determinate" value={progress} />
+                        <Typography variant="body2" color="textSecondary">
+                            {progress}%
+                        </Typography>
+                    </Box>
+
+                    {outputFile && (
+                        <Box sx={{ my: 2 }}>
+                            <Button
+                                variant="contained"
+                                color="success"
+                                startIcon={<SaveAltIcon />}
+                                href={outputFile}
+                                download={`${fileName}.xlsx`}
+                            >
+                                Tải xuống file Excel
+                            </Button>
+                        </Box>
+                    )}
+                </>
             )}
         </Container>
     );
